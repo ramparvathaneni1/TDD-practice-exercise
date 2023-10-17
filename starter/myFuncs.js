2ba4c681983271aa28626466ab60ad2614d0d2ce
Marc Wright <marc@generalassemb.ly>
2023-10-17 09:47:35 -0500

    add starter code to myFuncs.js

--- starter/myFuncs.js
+++ starter/myFuncs.js
@@ -1,3 +1,15 @@
+const {getUser} = require('./module/userAPI');
 
+const add = (a, b) => {
+    return a + b;
+}
 
-module.exports = { }
+const multiply = (a, b) => {
+    return a * b;
+}
+
+const getUserByID = async (userId) => {
+    return await getUser(userId);
+}
+
+module.exports = { add, multiply, getUserByID }
