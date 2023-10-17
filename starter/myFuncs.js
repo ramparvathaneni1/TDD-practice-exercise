9a71dc4c745c2024512646abf9312936fcaab772
Marc Wright <marc@generalassemb.ly>
2023-10-17 09:56:02 -0500

    update myFuncs

--- starter/myFuncs.js
+++ starter/myFuncs.js
@@ -1,15 +1,5 @@
-const {getUser} = require('./module/userAPI');
-
 const add = (a, b) => {
     return a + b;
 }
 
-const multiply = (a, b) => {
-    return a * b;
-}
-
-const getUserByID = async (userId) => {
-    return await getUser(userId);
-}
-
-module.exports = { add, multiply, getUserByID }
+module.exports = { add }
